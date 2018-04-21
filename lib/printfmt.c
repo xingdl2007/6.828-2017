398622917aa8424a254fd02f95225d15e43f405c
xingdl2007 <xingdl2007@gmail.com>
2018-04-21 14:28:43 +0800

    lab1: booting a pc: exercise 8 (remove prefix 0)

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -205,8 +205,6 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 
 		// (unsigned) octal
 		case 'o':
-			// Replace this with your code.
-			putch('0', putdat);
 			num = getuint(&ap, lflag);
 			base = 8;
 			goto number;
