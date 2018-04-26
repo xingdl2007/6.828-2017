784550dcbf9310c1ede17632e5ad151f15c74731
xingdl2007 <xingdl2007@gmail.com>
2018-04-26 15:48:58 +0800

    lab3: user environment: part A: exercise 2

--- kern/env.c
+++ kern/env.c
@@ -370,7 +370,7 @@ load_icode(struct Env *e, uint8_t *binary)
 		if(ph->p_type == ELF_PROG_LOAD) {
 			region_alloc(e, (void *)ph->p_va, ph->p_memsz);
 			memcpy((void *)ph->p_va, binary + ph->p_offset, ph->p_filesz);
-			memset((void *)ph->p_va + ph->p_filesz, 0,
+			memset((uint8_t *)ph->p_va + ph->p_filesz, 0,
 			       ph->p_memsz - ph->p_filesz);
 		}
 	}
