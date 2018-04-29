9fd7ac677576f51511ac1edeaafbd16a132dde97
xingdl2007 <xingdl2007@gmail.com>
2018-04-29 14:52:32 +0800

    lab4: preemptive multitasking: part A: exercise 5

--- kern/trap.c
+++ kern/trap.c
@@ -146,7 +146,7 @@ trap_init_percpu(void)
 
 	// Setup a TSS so that we get the right stack
 	// when we trap to the kernel.
-	thiscpu->cpu_ts.ts_esp0 = KSTACKTOP;
+	thiscpu->cpu_ts.ts_esp0 = KSTACKTOP - (thiscpu-cpus)*(KSTKSIZE+KSTKGAP);
 	thiscpu->cpu_ts.ts_ss0 = GD_KD;
 	thiscpu->cpu_ts.ts_iomb = sizeof(struct Taskstate);
 
