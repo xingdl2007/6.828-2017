2f0d8e032ffa3d20fdb1fa70b42283bdec8b710a
xingdl2007 <xingdl2007@gmail.com>
2018-04-29 14:46:30 +0800

    lab4: preemptive multitasking: part A: exercise 4

--- kern/pmap.c
+++ kern/pmap.c
@@ -275,7 +275,10 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
-
+	for(int i = 0; i < NCPU; ++i) {
+		boot_map_region(kern_pgdir, KSTACKTOP-(KSTKSIZE+KSTKGAP)*i-KSTKSIZE,
+				KSTKSIZE, PADDR(percpu_kstacks[i]), PTE_W);
+	}
 }
 
 // --------------------------------------------------------------
