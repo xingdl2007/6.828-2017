72ba8d172a2a06da9dce1dbc69ea3ffbef81202a
xingdl2007 <xingdl2007@gmail.com>
2018-04-30 12:09:32 +0800

    lab4: preemptive multitasking: part A: exercise 8

--- kern/syscall.c
+++ kern/syscall.c
@@ -140,7 +140,12 @@ static int
 sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
 	// LAB 4: Your code here.
-	panic("sys_env_set_pgfault_upcall not implemented");
+	int ret;
+	struct Env *env;
+	if((ret = envid2env(envid, &env, true)) < 0) {
+		return ret;
+	}
+	env->env_pgfault_upcall = func;
 }
 
 // Allocate a page of memory and map it at 'va' with permission
