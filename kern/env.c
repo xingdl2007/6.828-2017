9af36af36c6b683534cea958dba8243bd81f506a
xingdl2007 <xingdl2007@gmail.com>
2018-05-05 10:31:40 +0800

    lab5: file system, spawn and shell: exercise 1

--- kern/env.c
+++ kern/env.c
@@ -422,6 +422,9 @@ env_create(uint8_t *binary, enum EnvType type)
 		panic("env_create(): evn_alloc failed: %e\n", r);
 	}
 	e->env_type = type;
+	if(e->env_type == ENV_TYPE_FS) {
+		e->env_tf.tf_eflags |= FL_IOPL_3;
+	}
 	load_icode(e, binary);
 
 }
