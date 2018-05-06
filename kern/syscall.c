86b585611fc444af4fbd17357f4762a01b32720a
xingdl2007 <xingdl2007@gmail.com>
2018-05-06 12:21:29 +0800

    lab5: file system, spawn and shell: exercise 7

--- kern/syscall.c
+++ kern/syscall.c
@@ -137,7 +137,18 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+	struct Env *env;
+	int ret;
+	if((ret = envid2env(envid, &env, true)) < 0)
+		return ret;
+
+	user_mem_assert(curenv, tf, sizeof(struct Trapframe), PTE_U);
+
+	env->env_tf = *tf;
+	env->env_tf.tf_cs |= 3;
+	env->env_tf.tf_eflags &= ~FL_IOPL_MASK;
+	env->env_tf.tf_eflags |= FL_IF | FL_IOPL_0;
+	return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -426,6 +437,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return sys_exofork();
 	case SYS_env_set_status:
 		return sys_env_set_status(a1, a2);
+	case SYS_env_set_trapframe:
+		return sys_env_set_trapframe(a1, (struct Trapframe*)a2);
 	case SYS_env_set_pgfault_upcall:
 		return sys_env_set_pgfault_upcall(a1, (void *)a2);
 	case SYS_yield:
