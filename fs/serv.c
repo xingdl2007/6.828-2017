d34ea71eaa3b49f09fa37fe7f79499a0989da0ff
xingdl2007 <xingdl2007@gmail.com>
2018-05-06 11:26:46 +0800

    lab5: file system, spawn and shell: exercise 6

--- fs/serv.c
+++ fs/serv.c
@@ -236,7 +236,14 @@ serve_write(envid_t envid, struct Fsreq_write *req)
 		cprintf("serve_write %08x %08x %08x\n", envid, req->req_fileid, req->req_n);
 
 	// LAB 5: Your code here.
-	panic("serve_write not implemented");
+	struct OpenFile *o;
+	int r;
+	if((r = openfile_lookup(envid, req->req_fileid, &o)) < 0)
+		return r;
+	if((r = file_write(o->o_file, req->req_buf, req->req_n, o->o_fd->fd_offset)) < 0)
+		return r;
+	o->o_fd->fd_offset += r;
+	return r;
 }
 
 // Stat ipc->stat.req_fileid.  Return the file's struct Stat to the
